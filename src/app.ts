0e5901af75c98dc253b08269a55ebb4db0e63308
Afomike <temidayoafote@gmail.com>
2026-07-09 19:42:27 +0100

    update datatable

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
+  "http://192.168.0.125:5173",
   "https://seed-sower-lmc.vercel.app",
 ];
 
